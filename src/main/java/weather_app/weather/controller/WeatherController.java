c09328f4275b9ab683946ecbf9ff9b5a7d8afa4d
GeethmaMunasinghe <123428378+GeethmaMunasinghe@users.noreply.github.com>
2025-05-01 22:03:36 +0530

    WeatherResponse model create

--- src/main/java/weather_app/weather/controller/WeatherController.java
+++ src/main/java/weather_app/weather/controller/WeatherController.java
@@ -1,8 +1,26 @@
 package weather_app.weather.controller;
 
+import lombok.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
 
 @Controller
 public class WeatherController {
-    
+        @Value("${api.key}")
+        private String apiKey;
+
+        @GetMapping("/abc")
+        public String getIndex(){
+            return "index";
+        }
+
+        @GetMapping("/weather")
+        public String getWeather(@RequestParam ("city") String city, Model model){
+                String url="https://home.openweathermap.org/data/2.5/weather?q="+city+"&appId"+apiKey+"&units=metric";
+                RestTemplate restTemplate = new RestTemplate();
+        }
+
 }
